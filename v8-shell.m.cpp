d3c5be5b0c58aca2334946df46c79b109a3ccb62
Masud Rahman <masud.s.rahman@gmail.com>
2013-08-16 01:41:31 -0400

    Implement a REPL loop.

--- v8-shell.m.cpp
+++ v8-shell.m.cpp
@@ -5,16 +5,6 @@
 #include <iostream>
 #include <iterator>
 
-#define CHECK_RETURN(tryCatch)                                                \
-    do {                                                                      \
-        if (tryCatch.HasCaught()) {                                           \
-            v8::Handle<v8::Message> message = tryCatch.Message();             \
-            formatMessage(std::cerr, tryCatch.Message());                     \
-            return -1;                                                        \
-        }                                                                     \
-    }                                                                         \
-    while (false)
-
 namespace {
 
 static const char *toCString(v8::Handle<v8::Value> value)
@@ -22,8 +12,8 @@ static const char *toCString(v8::Handle<v8::Value> value)
     return *v8::String::Utf8Value(value);
 }
 
-static std::ostream& formatMessage(std::ostream&           stream,
-                                   v8::Handle<v8::Message> message)
+static std::ostream& format(std::ostream&           stream,
+                            v8::Handle<v8::Message> message)
 {
     stream << toCString(message->Get()) << '\n';
     v8::Handle<v8::StackTrace> stack = message->GetStackTrace();
@@ -44,16 +34,87 @@ static std::ostream& formatMessage(std::ostream&           stream,
     return stream;
 }
 
+static std::ostream& format(std::ostream&         stream,
+                            v8::Handle<v8::Value> value)
+{
+    return stream << toCString(value) << std::endl;
+}
+
+static int read(std::string *result, std::istream& in, std::ostream& out)
+{
+    result->clear();
+
+    out << ">>> ";
+    bool escape = false;
+    bool append = true;
+    while (true) {
+        std::istream::int_type character = in.get();
+        switch (character) {
+            case -1: {
+                out << "\n";
+            } return -1;
+
+            case '\n': {
+                if (escape) {
+                    out << "... ";
+                    escape = false;
+                }
+                else {
+                    return 0;
+                }
+            } break;
+
+            case '\\': {
+                escape = true;
+                append = false;
+            } break;
+        }
+        if (append) {
+            result->push_back(std::istream::traits_type::to_char_type(
+                                                                   character));
+        }
+        append = true;
+    }
+}
+
+static int evaluate(v8::Handle<v8::Value>   *result,
+                    v8::Handle<v8::Message> *errorMessage,
+                    const std::string&       input)
+{
+    // This will catch errors within this scope
+    v8::TryCatch tryCatch;
+
+    // Load the input string
+    v8::Handle<v8::String> source = v8::String::New(input.data(),
+                                                    input.length());
+    if (tryCatch.HasCaught()) {
+        *errorMessage = tryCatch.Message();
+        return -1;
+    }
+
+    // Compile it
+    v8::Handle<v8::Script> script = v8::Script::Compile(
+                                                   source,
+                                                   v8::String::New("<stdin>"));
+    if (tryCatch.HasCaught()) {
+        *errorMessage = tryCatch.Message();
+        return -1;
+    }
+
+    // Evaluate it
+    *result = script->Run();
+    if (tryCatch.HasCaught()) {
+        *errorMessage = tryCatch.Message();
+        return -1;
+    }
+
+    return 0;
+}
+
 }  // close unnamed namespace
 
 int main(int argc, char *argv[])
 {
-    // Read stdin into 'input'
-    std::string input;
-    std::copy(std::istreambuf_iterator<char>(std::cin.rdbuf()),
-              std::istreambuf_iterator<char>(),
-              std::back_inserter(input));
-
     // Initialize global v8 variables
     v8::Isolate *isolate = v8::Isolate::GetCurrent();
     v8::V8::SetCaptureStackTraceForUncaughtExceptions(
@@ -68,26 +129,18 @@ int main(int argc, char *argv[])
     v8::Handle<v8::Context> context = v8::Context::New(isolate);
     v8::Context::Scope      scope(context);
 
-    // This in combination with 'CHECK_RETURN' will do error checking
-    v8::TryCatch errors;
-
-    // Load the input string
-    v8::Handle<v8::String> source = v8::String::New(input.data(),
-                                                    input.length());
-    CHECK_RETURN(errors);
-
-    // Compile it
-    v8::Handle<v8::Script> script = v8::Script::Compile(
-                                                   source,
-                                                   v8::String::New("<stdin>"));
-    CHECK_RETURN(errors);
-
-    // Evaluate it
-    v8::Handle<v8::Value> result = script->Run();
-    CHECK_RETURN(errors);
-
-    // Print the result
-    std::cout << toCString(result) << std::endl;
+    std::string input;
+    while (read(&input, std::cin, std::cout) == 0) {
+        v8::Handle<v8::Value>   result;
+        v8::Handle<v8::Message> errorMessage;
+        if (evaluate(&result, &errorMessage, input)) {
+            format(std::cerr, errorMessage);
+        }
+        else {
+            std::cout << "-> ";
+            format(std::cout, result);
+        }
+    }
 
     return 0;
 }
